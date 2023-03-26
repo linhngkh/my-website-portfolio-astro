78e4df009d9181f46922539c4bb8aecc6447ca88
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-26 15:48:33 +0300

    delete unnecessary file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-modern-personal-website.netlify.app",
-  integrations: [ tailwind()],
+  site: "https://linh-nguyenkhanh.github.io/myPortfolioV3/",
+  integrations: [tailwind()],
 });
