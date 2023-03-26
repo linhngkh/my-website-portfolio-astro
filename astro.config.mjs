0bb93512835fcca14f6ae392441690a95cc15d58
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-26 18:04:14 +0300

    change config link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://linh-nguyenkhanh.github.io",
+  site: "https://linhngkh.github.io/linh-portfolio/",
 
   integrations: [tailwind()],
 });
