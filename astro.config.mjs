2033a7c3cbd7be031b587539e3f64cacd3a542bd
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-26 16:02:19 +0300

    change yml file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://linh-nguyenkhanh.github.io/myPortfolioV3/",
+  site: "",
   integrations: [tailwind()],
 });
