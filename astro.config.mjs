684a6ec8a4d6e17712dd071a0d95d0d6b3537961
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-03-27 08:05:00 +0300

    fix why web doesn show

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://linhngkh.github.io",
+  site: "https://linhngkh.github.io/linh-portfolio",
 
   integrations: [tailwind()],
 });
