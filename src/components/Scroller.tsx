f4e5d913e3b65da1a86df44ba625279435f79638
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-05-23 21:32:46 +0300

    style project section

--- src/components/Scroller.tsx
+++ src/components/Scroller.tsx
@@ -2,8 +2,17 @@ import Marquee from "react-fast-marquee";
 
 export default function Scroller() {
   return (
-    <div>
-      <Marquee>test</Marquee>
-    </div>
+    <section>
+      <a href="/contact">
+        <Marquee
+          pauseOnHover
+          gradient={false}
+          className="bg-yellow-300 border-t-2 border-black py-2 font-bold text-3xl"
+        >
+          🌟 Let's get to work! 🌟 Have a project in mind? 🌟 Let's get to work!
+          🌟 Have a project in mind?
+        </Marquee>
+      </a>
+    </section>
   );
 }
