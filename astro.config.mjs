ee9f2326f84887e2bd6991bca556a9fdf5aa1283
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-05-23 21:09:17 +0300

    build marquee component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
 
+
 // https://astro.build/config
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: netlify()
 });
---
+++ src/components/Scroller.tsx
@@ -0,0 +1,9 @@
+import Marquee from "react-fast-marquee";
+
+export default function Scroller() {
+  return (
+    <div>
+      <Marquee>test</Marquee>
+    </div>
+  );
+}
