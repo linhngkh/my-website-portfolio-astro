c0c1dc459e2168dd9f25941716a55c4cac72d24a
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-06-17 16:27:36 +0300

    update website

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 
-export const SITE_TITLE = "Linh's Portfolio Website";
+export const SITE_TITLE = "Zoe's Portfolio Website";
 export const SITE_DESCRIPTION = 'My Personal Portfolio Website made with Astro.';
