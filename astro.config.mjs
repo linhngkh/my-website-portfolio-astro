146f9af452ce509cc2cbba484a78c8fe916f1ab4
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-08-07 22:32:52 +0300

    playing with lottie ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
-
-
+import lottie from "astro-integration-lottie";
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), lottie()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
